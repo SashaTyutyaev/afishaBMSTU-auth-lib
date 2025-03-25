ce3bec7b8dbe141ae3df2958e0916e799218db31
SashaTyutyaev <sashatut2004@yandex.ru>
2025-03-25 23:03:16 +0300

    upd jwtService

--- src/main/java/afishaBMSTU/auth_lib/security/BaseJwtService.java
+++ src/main/java/afishaBMSTU/auth_lib/security/BaseJwtService.java
@@ -1,24 +1,19 @@
 package afishaBMSTU.auth_lib.security;
 
-import afishaBMSTU.auth_lib.security.dto.UserInfoDto;
 import afishaBMSTU.auth_lib.security.exception.IncorrectTokenException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
-import java.util.List;
 
 public abstract class BaseJwtService<T> {
 
     @Value("${security.jwt.secret}")
     private String secret;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public UserInfoDto<T> extractUserInfo(String token) {
+    public T extractUserInfo(String token) {
         if (token == null || !token.startsWith("Bearer")) {
             throw new IncorrectTokenException("Missing bearer prefix");
         }
@@ -29,17 +24,13 @@ public abstract class BaseJwtService<T> {
                 .parseClaimsJws(token.substring(7))
                 .getBody();
 
-        T data = objectMapper.convertValue(claims.get("data"), getDataType());
 
-        return UserInfoDto.<T>builder()
-                .data(data)
-                .roles(claims.get("roles", List.class))
-                .build();
+        return claims.get("data", getDataType());
     }
 
     protected abstract Class<T> getDataType();
 
-    protected String generateToken(T data, List<String> roles) {
+    protected String generateToken(T data) {
         return null;
     }
 
--- src/main/java/afishaBMSTU/auth_lib/security/dto/UserInfoDto.java
+++ src/main/java/afishaBMSTU/auth_lib/security/dto/UserInfoDto.java
@@ -4,12 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @Builder
 public class UserInfoDto<T> {
     private T data;
-    private List<String> roles;
 }
